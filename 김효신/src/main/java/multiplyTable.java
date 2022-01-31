bf73960cd36bf90030ce9b5531e26edbb310c42e
Shinnybest <kim295521@naver.com>
2022-01-31 21:32:41 +0900

    java

---
+++ 김효신/src/main/java/multiplyTable.java
@@ -0,0 +1,17 @@
+public class multiplyTable {
+    public static void main(String[] args) {
+        System.out.print("   |");
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println("\n---+---------------------------");
+
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%2d |", i);
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%3d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
